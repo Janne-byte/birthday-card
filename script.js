7637be140f9005db88f517c821f072283fb30cb1
Janne-byte <nyatungwale@gmail.com>
2024-10-26 00:43:40 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+document.getElementById("surpriseButton").addEventListener("click", function() {
+    document.getElementById("cakeGif").style.display = "block"; 
+    document.getElementById("hiddenMessage").style.display = "block"; 
+    document.getElementById("birthdaymusic").play();
+});
